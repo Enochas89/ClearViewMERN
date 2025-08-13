ec94a17247adec2e5d4d3943013da0cf52aeb868
Enochas89 <blitzkriegu@gmail.com>
2025-08-13 19:15:28 -0400

    Delete scripts directory

--- scripts/smoke.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-B=${1:-http://localhost:5000}
-for path in /api/meta /api/users /api/projects /api/tasks?projectId=p1; do
-  echo -e "\n==> GET $path"
-  curl -s "$B$path" | jq 'del(.ts) | .'
-done
