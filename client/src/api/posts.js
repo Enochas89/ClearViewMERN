a1608321997271042ef699535baa879c7d698517
Enochas89 <blitzkriegu@gmail.com>
2025-08-13 19:15:07 -0400

    Delete client directory

--- client/src/api/posts.js
+++
@@ -1,4 +0,0 @@
-import { api } from './axios'
-
-export const listPosts = async (projectId) => (await api.get('/api/posts', { params: { projectId } })).data
-export const likePost = async (postId) => (await api.post(`/api/posts/${postId}/like`)).data
--- client/src/api/projects.js
+++
@@ -1,4 +0,0 @@
-import { api } from './axios'
-
-export const listProjects = async () => (await api.get('/api/projects')).data
-export const getProject = async (id) => (await api.get(`/api/projects/${id}`)).data
